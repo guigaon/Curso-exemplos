9ee65f144ce40f951e7444748c54e5f02a91bcb2
guigaon <guilhermekirsch12@gmail.com>
2020-07-02 17:08:49 -0300

    crud finalizado

--- CRUD/crudPDO.php
+++ CRUD/crudPDO.php
@@ -1,4 +1,28 @@
 <?php
+function consultar ($sql, $parametro=array(), $todos=true, $modo=PDO::FETCH_ASSOC){
+	$conn = DB::getInstance()->getDb();
+	
+	
+	
+	$stmt= $conn->prepare($sql);
+	
+	foreach($parametro as $chave=>$valor){
+		$tipo = ((is_int($valor)) ? PDO::PARAM_INT : PDO::PARAM_STR);
+		$stmt->bindValue (":$chave", $valor, $tipo);	
+		
+	}
+	
+	$stmt->execute();
+	
+	if($todos){
+		return $stmt->fetchAll($modo);
+		
+	}else{
+		return $stmt->fetch($modo);
+		
+	}
+}
+
 
 
 function inserir($tabela, array $dados){
@@ -24,6 +48,7 @@ function inserir($tabela, array $dados){
 }
 
 function alterar($tabela, $dados, $condicao){
+	$conn = DB::getInstance()->getDb();
 	$parametros = null;
 	
 	foreach ($dados as $chave=>$valor){
@@ -34,9 +59,28 @@ function alterar($tabela, $dados, $condicao){
 	$parametros = rtrim($parametros, ",");//exclui o ultimo caracter, a virgula
 	
 	$sql = "UPDATE {$tabela} SET {$parametros} WHERE {$condicao}";
+	$stmt = $conn->prepare($sql);
 	
-return $sql;
+	foreach($dados as $chave=>$valor){
+		$tipo = ((is_int($valor)) ? PDO::PARAM_INT : PDO::PARAM_STR);
+		$stmt->bindValue (":$chave", $valor, $tipo);	
+		
+	}
+return $stmt->execute();
 	
+}
+
+function excluir ($tabela, $condicao = null){
+	$conn = DB::getInstance()->getDb();
+	
+	if($condicao!=null){
+		$condicao = " WHERE " .$condicao;
+			
+	}
+	$sql = "DELETE FROM {$tabela} {$condicao}";
+	$stmt = $conn->prepare($sql);
+	
+	return $stmt->execute();
 }
 
 
--- CRUD/index.php
+++ CRUD/index.php
@@ -4,7 +4,7 @@ include ("crudPDO.php");
 
 $conn = DB::getInstance(); //metodo estatico, nao precisa instanciar e foi instanciado direto na função
 
-$dados = array ("nome" => "gui", "email" => "gui@gmail.com", "fone" => "99999999");
+$dados = array ("nome" => "guilherme", "email" => "gui@gmail.com", "fone" => "99999999");
 
-print_r(alterar("cliente",$dados,"id_cliente = 2"));
+print_r(consultar("select * from cliente"));
 ?>
